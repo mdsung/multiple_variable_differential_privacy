a4aa7df25187505d6e617ede8564b5f4cbcc43ad
mdsung <mdskylover@gmail.com>
2021-12-06 16:29:38 +0900

    Create HR plot by epsilon with original survival analysis CI

--- visualize_cox_regression_beta_dp.R
+++ visualize_cox_regression_beta_dp.R
@@ -7,7 +7,7 @@ library(tools)
 original <- read_feather(here('output_cc/beta_original.feather'))
 dp <- read_feather(here('output_cc/beta_by_epsilon.feather'))
 
-original_variable <- original %>% select(variable, original_HR = HR)
+original_variable <- original %>% rename(original_HR = HR)
 
 summary_data <- dp %>% 
     group_by(variable, epsilon) %>% 
@@ -16,16 +16,17 @@ summary_data <- dp %>%
             se_HR = sd(HR) / n(), .groups = 'drop') %>%
     left_join(original_variable) %>%
     mutate(variable = factor(variable, levels = c('age', 'sex', 'stage4', 'antipsychotics', 'antidepressant', 'anxiolytic')))
-
+summary_data
 
 HR_figure <- summary_data %>%
     ggplot(aes(x = epsilon, y = mean_HR)) + 
         geom_point() + 
         geom_errorbar(aes(ymax = mean_HR + sd_HR, ymin = mean_HR - sd_HR), width = 0.1) + 
         geom_hline(aes(yintercept = original_HR), linetype = 'dashed', color = 'red') + 
+        geom_rect(aes(xmin = 0, xmax = 7, ymin = lowerCI, ymax = upperCI), fill ="gray70", alpha = 0.05)+ 
         labs(x = "Epsilon", y = "HR", title = "Cox regression HR by Epsilon in Vertical Partitioned Data")+ 
         theme_classic() + 
         facet_wrap(~variable, scales = 'free_y')
-
+HR_figure
 ggsave(plot = HR_figure, filename = here('images/HR_by_epsilon.png'), height = 8, width = 8, dpi = 300)
 
